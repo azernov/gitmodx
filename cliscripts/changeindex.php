65d390c208d980c931046b70fd0b1821ed65ee15
artprog <aprog-art@mail.ru>
2020-01-13 10:39:59 +0800

    Update script for automatic replace paths in core files

--- cliscripts/changeindex.php
+++ cliscripts/changeindex.php
@@ -2,7 +2,7 @@
 <?php
 
 /**
- * Automaticaly replace include path and class name to gitmodx in index.php files
+ * Automaticaly replace include path and class name to gitmodx in index.php files and change absolute path in core.inc.php files
  */
 
 include dirname(dirname(dirname(dirname(dirname(__FILE__))))).'/'.'config.core.php';
@@ -18,5 +18,17 @@ foreach($files as $file){
     $content = file_get_contents($file);
     $content = str_replace('model/modx/modx.class.php','components/gitmodx/model/gitmodx/gitmodx.class.php',$content);
     $content = str_replace('new modX(','new gitModx(',$content);
+    file_put_contents($file,$content);
+}
+
+$coreIncFiles = array(
+    MODX_BASE_PATH.'config.core.php' => "__DIR__.'/core/'",
+    MODX_CONNECTORS_PATH.'config.core.php' => "dirname(__DIR__).'/core/'",
+    MODX_MANAGER_PATH.'config.core.php' => "dirname(__DIR__).'/core/'",
+);
+
+foreach($coreIncFiles as $file => $replace){
+    $content = file_get_contents($file);
+    $content = preg_replace("#define\('MODX_CORE_PATH', '([^']+)'\);#mu", "define('MODX_CORE_PATH', {$replace});", $content);
     file_put_contents($file,$content);
 }
